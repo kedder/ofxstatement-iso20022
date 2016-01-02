f43ad13fc3861c54b3894621a3cc416c57716a40
Andrey Lebedev <andrey@lebedev.lt>
2016-01-02 10:35:12 +0200

    Initial implementation for ISO20022 parser
    Very trivial and naive for now

--- src/ofxstatement/plugins/iso20022.py
+++ src/ofxstatement/plugins/iso20022.py
@@ -1,23 +1,128 @@
+import xml.etree.ElementTree as ET
+import datetime
+
 from ofxstatement.plugin import Plugin
-from ofxstatement.parser import StatementParser
-from ofxstatement.statement import StatementLine
+from ofxstatement.statement import Statement, StatementLine
 
 
-class Iso2022Plugin(Plugin):
+XMLNS = {
+    's': 'urn:iso:std:iso:20022:tech:xsd:camt.053.001.02'
+}
+
+CD_CREDIT = 'CRDT'
+CD_DEBIT = 'DBIT'
+
+class Iso20022Plugin(Plugin):
     """ISO-20022 plugin
     """
 
     def getParser(self, filename):
-        return Iso2022Parser(filename)
+        return Iso20022Parser(filename)
 
 
-class Iso2022Parser(object):
+class Iso20022Parser(object):
     def __init__(self, filename):
         self.filename = filename
 
     def parse(self):
         """Main entry point for parsers
         """
-        with open(self.filename, "r") as f:
-            self.input = f
-            pass
+        self.statement = Statement()
+        tree = ET.parse(self.filename)
+
+        self._parse_statement_properties(tree)
+        self._parse_lines(tree)
+
+        return self.statement
+
+    def _parse_statement_properties(self, tree):
+        stmt = tree.find('./s:BkToCstmrStmt/s:Stmt', XMLNS)
+
+        bnk = stmt.find('./s:Acct/s:Svcr/s:FinInstnId/s:BIC', XMLNS)
+        iban =stmt.find('./s:Acct/s:Id/s:IBAN', XMLNS)
+        ccy =stmt.find('./s:Acct/s:Ccy', XMLNS)
+        bals = stmt.findall('./s:Bal', XMLNS)
+
+        bal_amts = {}
+        bal_dates = {}
+        for bal in bals:
+            cd = bal.find('./s:Tp/s:CdOrPrtry/s:Cd', XMLNS)
+            amt = bal.find('./s:Amt', XMLNS)
+            dt = bal.find('./s:Dt', XMLNS)
+
+            # Amount currency should match with statement currency
+            bal_amts[cd.text] = self._parse_amount(amt, ccy.text)
+            bal_dates[cd.text] = self._parse_date(dt)
+
+        self.statement.currency = ccy.text
+        self.statement.bank_id = bnk.text
+        self.statement.account_id = iban.text
+        self.statement.start_balance = bal_amts['OPBD']
+        self.statement.start_date = bal_dates['OPBD']
+        self.statement.end_balance = bal_amts['CLBD']
+        self.statement.end_date = bal_dates['CLBD']
+
+    def _parse_lines(self, tree):
+        for ntry in _findall(tree, 'BkToCstmrStmt/Stmt/Ntry'):
+            sline = self._parse_line(ntry)
+            self.statement.lines.append(sline)
+
+    def _parse_line(self, ntry):
+        sline = StatementLine()
+
+        crdeb = _find(ntry, 'CdtDbtInd').text
+
+        amtnode = _find(ntry, 'Amt')
+        amt = self._parse_amount(amtnode, self.statement.currency)
+        if crdeb == CD_DEBIT:
+            amt = -amt
+        sline.amount = amt
+
+        dt = _find(ntry, 'ValDt')
+        sline.date = self._parse_date(dt)
+
+        bookdt = _find(ntry, 'BookgDt')
+        sline.date_user = self._parse_date(bookdt)
+
+        svcref = _find(ntry, 'NtryDtls/TxDtls/Refs/AcctSvcrRef')
+        sline.refnum = svcref.text
+
+        rmtinf = _find(ntry, 'NtryDtls/TxDtls/RmtInf/Ustrd')
+        sline.memo = rmtinf.text
+
+        cdtr = _find(ntry, 'NtryDtls/TxDtls/RltdPties/Cdtr/Nm')
+        sline.payee = cdtr.text
+
+        return sline
+
+    def _parse_date(self, dtnode):
+        if dtnode is None:
+            return None
+
+        dt = _find(dtnode, 'Dt')
+        dttm = _find(dtnode, 'DtTm')
+
+        if dt is not None:
+            return datetime.datetime.strptime(dt.text, "%Y-%m-%d")
+        else:
+            assert dttm is not None
+            return datetime.datetime.strptime(dttm.text, "%Y-%m-%dT%H:%M:%S")
+
+    def _parse_amount(self, amtnode, currency):
+        assert amtnode.get('Ccy') == currency
+        return float(amtnode.text)
+
+
+def _toxpath(spath):
+    tags = spath.split('/')
+    path = ['s:%s' % t for t in tags]
+    xpath = './%s' % '/'.join(path)
+    return xpath
+
+
+def _find(tree, spath):
+    return tree.find(_toxpath(spath), XMLNS)
+
+
+def _findall(tree, spath):
+    return tree.findall(_toxpath(spath), XMLNS)
---
+++ src/ofxstatement/plugins/tests/test_iso20022.py
@@ -0,0 +1,44 @@
+import os
+import datetime
+
+from ofxstatement.ui import UI
+
+from ofxstatement.plugins.iso20022 import Iso20022Plugin
+
+
+HERE = os.path.dirname(__file__)
+SAMPLES_DIR = os.path.join(HERE, 'samples')
+
+
+def test_parse_simple():
+    # GIVEN
+    plugin = Iso20022Plugin(UI(), {})
+
+    parser = plugin.getParser(os.path.join(SAMPLES_DIR, 'simple.xml'))
+
+    # WHEN
+    stmt = parser.parse()
+
+    # THEN
+    assert stmt is not None
+
+    assert stmt.account_id == 'LT000000000000000000'
+    assert stmt.currency == 'EUR'
+    assert stmt.bank_id == 'AGBLLT2XXXX'
+    assert stmt.end_balance == 125.52
+    assert stmt.end_date == datetime.datetime(2015, 12, 31, 0, 0)
+    assert stmt.start_balance == 306.53
+    assert stmt.end_date == datetime.datetime(2015, 12, 31, 0, 0)
+
+    assert len(stmt.lines) == 4
+
+    assert all(l.amount for l in stmt.lines)
+
+    line0 = stmt.lines[0]
+
+    assert line0.amount == -0.29
+    assert line0.memo == u'Sąskaitos aptarnavimo mokestis'
+    assert line0.date == datetime.datetime(2016, 1, 1, 0, 0)
+    assert line0.date_user == datetime.datetime(2015, 12, 31, 0, 0)
+    assert line0.payee == u'AB DNB Bankas'
+    assert line0.refnum == 'FC1261858984'
