feaf2ec7695f92f19a356f157446c7ecdf0002cd
Andrey Lebedev <andrey@lebedev.lt>
2017-04-13 22:39:06 +0300

    Support another variety of ISO20022
    Support different XML namespaces, weird date formats, different
    places to store bank identification.
    
    Fixes #1.

--- src/ofxstatement/plugins/iso20022.py
+++ src/ofxstatement/plugins/iso20022.py
@@ -1,14 +1,13 @@
 import xml.etree.ElementTree as ET
 import datetime
+import re
 
 from ofxstatement import exceptions
 from ofxstatement.plugin import Plugin
 from ofxstatement.statement import Statement, StatementLine
 
 
-XMLNS = {
-    's': 'urn:iso:std:iso:20022:tech:xsd:camt.053.001.02'
-}
+ISO20022_NAMESPACE_ROOT = 'urn:iso:std:iso:20022:tech:xsd:camt.053.001'
 
 CD_CREDIT = 'CRDT'
 CD_DEBIT = 'DBIT'
@@ -35,18 +34,34 @@ class Iso20022Parser(object):
         self.statement.currency = self.currency
         tree = ET.parse(self.filename)
 
+        # Find out XML namespace and make sure we can parse it
+        ns = self._get_namespace(tree.getroot())
+        if not ns.startswith(ISO20022_NAMESPACE_ROOT):
+            raise ParseError("Cannot recognize ISO20022 XML")
+
+        self.xmlns = {
+            "s": ns
+        }
+
+
         self._parse_statement_properties(tree)
         self._parse_lines(tree)
 
         return self.statement
 
-    def _parse_statement_properties(self, tree):
-        stmt = tree.find('./s:BkToCstmrStmt/s:Stmt', XMLNS)
+    def _get_namespace(self, elem):
+        m = re.match('\{(.*)\}', elem.tag)
+        return m.groups()[0] if m else ''
 
-        bnk = stmt.find('./s:Acct/s:Svcr/s:FinInstnId/s:BIC', XMLNS)
-        iban =stmt.find('./s:Acct/s:Id/s:IBAN', XMLNS)
-        ccy =stmt.find('./s:Acct/s:Ccy', XMLNS)
-        bals = stmt.findall('./s:Bal', XMLNS)
+    def _parse_statement_properties(self, tree):
+        stmt = tree.find('./s:BkToCstmrStmt/s:Stmt', self.xmlns)
+
+        bnk = stmt.find('./s:Acct/s:Svcr/s:FinInstnId/s:BIC', self.xmlns)
+        if bnk is None:
+            bnk = stmt.find('./s:Acct/s:Svcr/s:FinInstnId/s:Nm', self.xmlns)
+        iban = stmt.find('./s:Acct/s:Id/s:IBAN', self.xmlns)
+        ccy = stmt.find('./s:Acct/s:Ccy', self.xmlns)
+        bals = stmt.findall('./s:Bal', self.xmlns)
 
         acctCurrency = ccy.text if ccy is not None else None
         if acctCurrency:
@@ -60,9 +75,9 @@ class Iso20022Parser(object):
         bal_amts = {}
         bal_dates = {}
         for bal in bals:
-            cd = bal.find('./s:Tp/s:CdOrPrtry/s:Cd', XMLNS)
-            amt = bal.find('./s:Amt', XMLNS)
-            dt = bal.find('./s:Dt', XMLNS)
+            cd = bal.find('./s:Tp/s:CdOrPrtry/s:Cd', self.xmlns)
+            amt = bal.find('./s:Amt', self.xmlns)
+            dt = bal.find('./s:Dt', self.xmlns)
             amt_ccy = amt.get('Ccy')
             # Amount currency should match with statement currency
             if amt_ccy != self.statement.currency:
@@ -84,7 +99,7 @@ class Iso20022Parser(object):
         self.statement.end_date = bal_dates['CLBD']
 
     def _parse_lines(self, tree):
-        for ntry in _findall(tree, 'BkToCstmrStmt/Stmt/Ntry'):
+        for ntry in self._findall(tree, 'BkToCstmrStmt/Stmt/Ntry'):
             sline = self._parse_line(ntry)
             if sline is not None:
                 self.statement.lines.append(sline)
@@ -92,9 +107,9 @@ class Iso20022Parser(object):
     def _parse_line(self, ntry):
         sline = StatementLine()
 
-        crdeb = _find(ntry, 'CdtDbtInd').text
+        crdeb = self._find(ntry, 'CdtDbtInd').text
 
-        amtnode = _find(ntry, 'Amt')
+        amtnode = self._find(ntry, 'Amt')
         amt_ccy = amtnode.get('Ccy')
 
         if amt_ccy != self.statement.currency:
@@ -105,25 +120,27 @@ class Iso20022Parser(object):
         amt = self._parse_amount(amtnode)
         if crdeb == CD_DEBIT:
             amt = -amt
-            payee = _find(ntry, 'NtryDtls/TxDtls/RltdPties/Cdtr/Nm')
+            payee = self._find(ntry, 'NtryDtls/TxDtls/RltdPties/Cdtr/Nm')
         else:
-            payee = _find(ntry, 'NtryDtls/TxDtls/RltdPties/Dbtr/Nm')
+            payee = self._find(ntry, 'NtryDtls/TxDtls/RltdPties/Dbtr/Nm')
 
         sline.payee = payee.text if payee is not None else None
         sline.amount = amt
 
-        dt = _find(ntry, 'ValDt')
+        dt = self._find(ntry, 'ValDt')
         sline.date = self._parse_date(dt)
 
-        bookdt = _find(ntry, 'BookgDt')
+        bookdt = self._find(ntry, 'BookgDt')
         sline.date_user = self._parse_date(bookdt)
 
-        svcref = _find(ntry, 'NtryDtls/TxDtls/Refs/AcctSvcrRef')
+        svcref = self._find(ntry, 'NtryDtls/TxDtls/Refs/AcctSvcrRef')
+        if svcref is None:
+            svcref = self._find(ntry, 'AcctSvcrRef')
         sline.refnum = svcref.text
 
         # Try to find memo from different possible locations
-        rmtinf = _find(ntry, 'NtryDtls/TxDtls/RmtInf/Ustrd')
-        addinf = _find(ntry, 'AddtlNtryInf')
+        rmtinf = self._find(ntry, 'NtryDtls/TxDtls/RmtInf/Ustrd')
+        addinf = self._find(ntry, 'AddtlNtryInf')
         if rmtinf is not None:
             sline.memo = rmtinf.text
         elif addinf is not None:
@@ -135,18 +152,34 @@ class Iso20022Parser(object):
         if dtnode is None:
             return None
 
-        dt = _find(dtnode, 'Dt')
-        dttm = _find(dtnode, 'DtTm')
+        dt = self._find(dtnode, 'Dt')
+        dttm = self._find(dtnode, 'DtTm')
 
         if dt is not None:
-            return datetime.datetime.strptime(dt.text, "%Y-%m-%d")
+            dtvalue = self._notimezone(dt.text)
+            return datetime.datetime.strptime(dtvalue, "%Y-%m-%d")
         else:
             assert dttm is not None
-            return datetime.datetime.strptime(dttm.text, "%Y-%m-%dT%H:%M:%S")
+            dtvalue = self._notimezone(dttm.text)
+            return datetime.datetime.strptime(dtvalue, "%Y-%m-%dT%H:%M:%S")
+
+    def _notimezone(self, dt):
+        # Sometimes we are getting time with ridiculous timezone, like
+        # "2017-04-01+02:00", which is unparseable by any python parsers. Strip
+        # out such timezone for good.
+        if "+" not in dt:
+            return dt
+        dt, tz = dt.split("+")
+        return dt
 
     def _parse_amount(self, amtnode):
         return float(amtnode.text)
 
+    def _find(self, tree, spath):
+        return tree.find(_toxpath(spath), self.xmlns)
+
+    def _findall(self, tree, spath):
+        return tree.findall(_toxpath(spath), self.xmlns)
 
 def _toxpath(spath):
     tags = spath.split('/')
@@ -154,10 +187,3 @@ def _toxpath(spath):
     xpath = './%s' % '/'.join(path)
     return xpath
 
-
-def _find(tree, spath):
-    return tree.find(_toxpath(spath), XMLNS)
-
-
-def _findall(tree, spath):
-    return tree.findall(_toxpath(spath), XMLNS)
--- tests/test_iso20022.py
+++ tests/test_iso20022.py
@@ -93,3 +93,41 @@ def test_parse_gcamp6():
     assert line0.date_user == datetime.datetime(2016, 4, 23, 0, 0)
     assert line0.payee is None
     assert line0.refnum == '20160423000805545979476000000012'
+
+
+def test_parse_davider80():
+    # GIVEN
+    config = {
+        "currency": "CHF"
+    }
+    plugin = Iso20022Plugin(UI(), config)
+
+    parser = plugin.get_parser(os.path.join(SAMPLES_DIR, 'davider80.xml'))
+
+    # WHEN
+    stmt = parser.parse()
+
+    # THEN
+    assert stmt is not None
+
+    assert stmt.account_id == 'CHxxxxxxxxxxxxxxxxxxx'
+    assert stmt.currency is "CHF"
+
+    assert stmt.bank_id == "Raiffeisen"
+    assert stmt.end_balance == 5753.46
+    assert stmt.end_date == datetime.datetime(2017, 4, 11, 0, 0)
+    assert stmt.start_balance == 9433.31
+    assert stmt.start_date == datetime.datetime(2017, 4, 1, 0, 0)
+
+    assert len(stmt.lines) == 16
+
+    assert all(l.amount for l in stmt.lines)
+
+    line0 = stmt.lines[0]
+
+    assert line0.amount == -905.3
+    assert line0.memo == 'Sistema di addebitamento diretto xxxxxxxxxxxxxxxxxxxxxx AG'
+    assert line0.date == datetime.datetime(2017, 4, 3, 0, 0)
+    assert line0.date_user == datetime.datetime(2017, 4, 1, 0, 0)
+    assert line0.payee is None
+    assert line0.refnum == '210564431020000000024556150000'
